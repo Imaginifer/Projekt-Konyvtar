fee85fe3f4955e5671943ef011f6b50f727085ba
Bence Cserfalvi <bence.cserfalvi@gmail.com>
2019-06-05 13:08:27 +0200

    BugFix

--- projekt-konyvtar/src/projekt/konyvtar/ProjektKonyvtar.java
+++ projekt-konyvtar/src/projekt/konyvtar/ProjektKonyvtar.java
@@ -15,7 +15,8 @@ public class ProjektKonyvtar {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        UserInt ui = new UserInt();
+        ui.mainMenu();
     }
     
 }
